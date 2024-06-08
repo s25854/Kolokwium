76abf3c096cb27f9545e51fc6022654940276d27
s25854 <s25854@pjwstk.edu.pl>
2024-06-08 18:33:01 +0200

    Commit

--- Context/Context.cs
+++ Context/Context.cs
@@ -19,6 +19,5 @@ public class Context: DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(Program).Assembly);
-        
     }
 }
